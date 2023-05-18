5b8b81d1531fc01e139466921cbdadd4123f9b46
Sudipto Das <shiningsudipto@gmail.com>
2023-05-18 22:05:50 +0600

    getting data by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -45,6 +45,14 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.findOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
